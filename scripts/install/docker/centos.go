9aa7eb94b6723a35be4703ca4a88260ebfd26d42
williamotieno <jimmywilliamotieno@gmail.com>
2023-07-19 09:39:36 +0300

    fix scripts

---
+++ scripts/install/docker/centos.go
@@ -0,0 +1,18 @@
+package docker
+
+func GetCentosCommands() []string {
+	commands := []string{
+		"sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine",
+		"sudo yum install -y yum-utils",
+		"sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
+		"sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+		"sudo systemctl enable docker",
+		"sudo systemctl start docker",
+		"sudo groupadd docker",
+		"sudo usermod -aG docker $USER",
+		"newgrp docker",
+		"docker run hello-world",
+	}
+
+	return commands
+}
---
+++ scripts/install/docker/test.go
@@ -0,0 +1,8 @@
+package docker
+
+func GetTestCommands() []string {
+	commands := []string{
+		"echo \"This is a test script. It worked!!!\"",
+	}
+	return commands
+}
