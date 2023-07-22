0de7be6b2ffd36bb91a6c0d3d63bc67d9df60cfe
williamotieno <jimmywilliamotieno@gmail.com>
2023-07-22 08:08:10 +0300

    patch

--- scripts/install/docker/centos.go
+++ scripts/install/docker/centos.go
@@ -5,7 +5,7 @@ func GetCentosCommands() []string {
 		"sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine",
 		"sudo yum install -y yum-utils",
 		"sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
-		"sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+		"sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
 		"sudo systemctl enable docker",
 		"sudo systemctl start docker",
 		"sudo groupadd docker",
--- scripts/install/docker/fedora.go
+++ scripts/install/docker/fedora.go
@@ -5,7 +5,7 @@ func GetFedoraCommands() []string {
 		"sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine",
 		"sudo dnf -y install dnf-plugins-core",
 		"sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo",
-		"sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+		"sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
 		"sudo systemctl enable docker",
 		"sudo systemctl start docker",
 		"sudo groupadd docker",
